825c49736aa0a1b602ebd9fe92b06cb95cc7b40b
Peedroca <phm1080@gmail.com>
2021-04-01 15:18:13 -0300

    teste com hostedservice

---
+++ src/TimeTetst/TimedHostedService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimeTetst
+{
+    public class TimedHostedService : IHostedService
+    {
+        private readonly ILogger<TimedHostedService> _logger;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+            return Task.CompletedTask;
+        }
+    }
+}
